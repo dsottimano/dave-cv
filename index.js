09486d57b55023813d8d3f1ae4d6faf120636bac
dsottimano <dsottimano@gmail.com>
2017-08-16 17:29:36 +0200

    hello

---
+++ index.js
@@ -0,0 +1,9 @@
+let express = require('express')
+let app = express()
+let bodyParser = require('body-parser')
+const PORT = process.env.PORT || 8080
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.listen(8080, function () {
+console.log('node is working')
+})
